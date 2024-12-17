8a3b8ff6fbdfce50706513e05651299c5e9b9fbf
Venkata Mahesh Babu Korrapati <korrapav@mail.gvsu.edu>
2024-12-16 18:23:47 -0600

    Main class name change

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UniversityApiApp {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UniversityApiApp.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
